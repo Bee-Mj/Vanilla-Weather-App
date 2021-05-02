dbbcd35d1ddf3d171013f8ab8539c1aa09ad2311
Bee-Mj <82813508+Bee-Mj@users.noreply.github.com>
2021-05-02 02:42:41 +0200

    Updates + added JS

--- src/app.js
+++ src/app.js
@@ -1,45 +1,8 @@
-   let now = new Date();
-
-    let h6 = document.querySelector("h6")
-
-    let date = now.getDate();
-    let hours = now.getHours();
-    let minutes = now.getMinutes();
-    let year = now.getFullYear();
-
-    let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
-    let day = days[now.getDay()];
-
-    let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-    let month = months[now.getMonth()];
-
-    h6.innerHTML = `${day} ${month} ${date}, ${hours}:${minutes}, ${year}`;
-
-
-
-
-
-
- function displayWeatherCondition(response) {
-     document.querySelector("#temperature").innerHTML = Math.round(response.data.main.temp);
- }
-
- function search(event) {
-    event.preventDefault();
-     let apiKey = "66df4c660e46f41bd29095093e07e243";
-     let city = document.querySelector("#search-text-input").value;
-     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-     axios.get(apiUrl).then(displayWeatherCondition);
-     let searchInput = document.querySelector("#search-text-input");
-     let h1 = document.querySelector("h1");
-     h1.innerHTML = `${searchInput.value}`;
+function displayTemperature(response) {
+   console.log(response.data);
 }
 
- let form = document.querySelector
- ("#search-engine");
+let apiKey = "66df4c660e46f41bd29095093e07e243";
+ let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
 
- form.addEventListener("submit", search);
-
-
-
-
+ axios.get(apiUrl).then(displayTemperature);
