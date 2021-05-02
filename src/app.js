e80b8b9c17d364e57ef01d2a9246f2386738e160
Bee-Mj <82813508+Bee-Mj@users.noreply.github.com>
2021-05-02 17:37:38 +0200

    Changed weather icon

--- src/app.js
+++ src/app.js
@@ -22,6 +22,7 @@ let descriptionElement = document.querySelector("#description");
 let humidityElement = document.querySelector("#humidity");
 let windElement = document.querySelector("#wind");
 let dateElement = document.querySelector("#date");
+let iconElement = document. querySelector("#icon");
 
 temperatureElement.innerHTML = Math.round (response.data.main.temp);
 cityElement.innerHTML = response.data.name;
@@ -29,10 +30,12 @@ descriptionElement.innerHTML = response.data.weather[0].description;
 humidityElement.innerHTML = response.data.main.humidity;
 windElement.innerHTML = Math.round(response.data.wind.speed);
 dateElement.innerHTML = formatDate(response.data.dt * 1000);
+iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`); 
+iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "66df4c660e46f41bd29095093e07e243";
-let city = "London";
+let city = "Paris";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
