e3580f3423f1061a8474b2a079428f167b397007
Bee-Mj <82813508+Bee-Mj@users.noreply.github.com>
2021-05-04 14:58:35 +0200

    Added all 5 days to forecast. JS

--- src/app.js
+++ src/app.js
@@ -14,6 +14,38 @@
    return `${day} ${hours}:${minutes}`;
    }
 
+function displayForecast() {
+let forecastElement = document.querySelector("#forecast");
+
+let forecastHTML = `<div class="row">`;
+let days = ["Tue", "Wed", "Thur", "Fri", "Sat"];
+days.forEach(function(day) {
+   forecastHTML = forecastHTML + 
+`
+   <div class="col-2">
+      <div class="weather-forecast-day">${day}</div>
+      <img src="http://openweathermap.org/img/wn/02d@2x.png" 
+      alt=""
+      width="38"
+      >
+      <div class="weather-forecast-temperature">
+         <span class="weather-forecast-temperature-max">18°</span> 
+         <span class="weather-forecast-temperature-low">15°</span>
+      </div>
+   </div>
+`;
+});
+
+
+
+forecastHTML = forecastHTML + `</div>`;
+forecastElement.innerHTML = forecastHTML;
+}
+
+
+
+
+
 
 function displayTemperature(response) {
 let temperatureElement = document.querySelector("#temperature");
@@ -78,3 +110,5 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("London");
+
+displayForecast();
